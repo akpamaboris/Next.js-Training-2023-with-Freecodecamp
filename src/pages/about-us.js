a0951a383ebb2d9bd601f4e719a41b4e20868d35
AkpamaBoris <zeliwipin@gmail.com>
2023-01-21 22:45:16 +0100

    first commit

---
+++ src/pages/about-us.js
@@ -0,0 +1,22 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>About Us Page</h1>
+      <p>
+        Welcome to our event site, your one-stop destination for all the best
+        events happening in London, San Francisco and Barcelona. We are
+        dedicated to providing you with the most comprehensive and up-to-date
+        information on all the hottest events in these great cities. From
+        world-renowned music festivals to delicious food and wine events, we've
+        got it all. Our team of experts scour the city to bring you the best
+        events and we are constantly updating our website to make sure you're in
+        the know about all the latest happenings. With our easy-to-use search
+        and filter options, you can find exactly what you're looking for in no
+        time. So whether you're a local or a tourist, join us and make the most
+        of your time in these amazing cities.
+      </p>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/pages/events/event1.js
@@ -0,0 +1,18 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Events 1 Page</h1>
+      <p>
+        Event Name: "London International Music Festival" Event Date: "July
+        15-20, 2022" Event Location: "Hyde Park, London, UK" Event Description:
+        "Join us for a celebration of music from all around the world at
+        London's premier outdoor music event. Enjoy performances by
+        world-renowned artists and up-and-coming talent on multiple stages
+        throughout the beautiful Hyde Park. Food and drink vendors will also be
+        on site. Don't miss out on this unforgettable experience!"
+      </p>
+    </div>
+  );
+};
+
+export default Page;
